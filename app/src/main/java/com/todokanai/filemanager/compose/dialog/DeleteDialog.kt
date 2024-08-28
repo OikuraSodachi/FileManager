1150872d90c9539a5668c17df8939ad3cf1dfd07
Todokanai <bh951201@gmail.com>
2024-08-28 10:44:00 +0900

    Compose ViewModel 제거 작업 진행중

--- app/src/main/java/com/todokanai/filemanager/compose/dialog/DeleteDialog.kt
+++ app/src/main/java/com/todokanai/filemanager/compose/dialog/DeleteDialog.kt
@@ -3,20 +3,18 @@ package com.todokanai.filemanager.compose.dialog
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.todokanai.filemanager.R
 import com.todokanai.filemanager.compose.presets.dialog.BooleanDialog
-import com.todokanai.filemanager.viewmodel.compose.dialog.DeleteDialogViewModel
 
 @Composable
 fun DeleteDialog(
     modifier: Modifier = Modifier,
     onCancel: () -> Unit,
-    viewModel: DeleteDialogViewModel = hiltViewModel()
+    delete:()->Unit
 ){
     BooleanDialog(
         modifier = modifier,
-        onConfirm = { viewModel.delete() },
+        onConfirm = { delete() },
         onCancel = { onCancel() },
         title = stringResource(id = R.string.delete_dialog_title),
         message = stringResource(id = R.string.delete_dialog_message)
--- app/src/main/java/com/todokanai/filemanager/compose/dialog/RenameDialog.kt
+++ app/src/main/java/com/todokanai/filemanager/compose/dialog/RenameDialog.kt
@@ -3,22 +3,21 @@ package com.todokanai.filemanager.compose.dialog
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.todokanai.filemanager.R
 import com.todokanai.filemanager.compose.presets.dialog.EditTextDialog
-import com.todokanai.filemanager.viewmodel.compose.dialog.RenameDialogViewModel
 
 @Composable
 fun RenameDialog(
     modifier: Modifier = Modifier,
     onCancel:()->Unit,
-    viewModel:RenameDialogViewModel = hiltViewModel()
+    //viewModel:RenameDialogViewModel = hiltViewModel()
+    onConfirm:(String)->Unit
 ){
     EditTextDialog(
         modifier = modifier,
         title = stringResource(id = R.string.rename_dialog_title),
         defaultText = stringResource(id = R.string.rename_dialog_default_text),
-        onConfirm = {viewModel.onConfirm(it)},
+        onConfirm = {onConfirm(it)},
         onCancel = {onCancel()}
     )
 }
