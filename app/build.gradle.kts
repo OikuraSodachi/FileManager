24a837fde4232015a284fe9e161534fa31f44594
Todokanai <bh951201@gmail.com>
2024-09-23 17:44:56 +0900

    onComplete Notification이 가끔 씹히는 문제 있음 (notification을 시스템에 요청하는 순서와, 실제 발생하는 순서가 반드시 일치하지는 않는듯?)
    UnzipAction 진행중
    //----------
    
    drag select 기능은 아마 이걸로? :
    
    https://github.com/KangTaeJong98/Example/tree/main/Android/RecyclerView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 
     buildFeatures {
         compose = true
-        dataBinding = true
         viewBinding = true
     }
     composeOptions {
--- app/src/main/java/com/todokanai/filemanager/test/Selection.kt
+++
@@ -1,6 +0,0 @@
-package com.todokanai.filemanager.test
-
-data class Selection(
-    var id: Long = 0L,
-    var text: String = ""
-)
