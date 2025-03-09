4010d9e43b9b1a5efad7c1d893331a5c93bf45d8
Todokanai <bh951201@gmail.com>
2025-03-09 10:09:36 +0900

    longClick-drag 로 MultiSelect 시작할 경우 Bottom Menu 나오지 않는 issue 있음
    NetFragment 작업중
    
    Safe

--- app/src/main/java/com/todokanai/filemanager/components/fragments/NetFragment.kt
+++ app/src/main/java/com/todokanai/filemanager/components/fragments/NetFragment.kt
@@ -1,12 +1,14 @@
 package com.todokanai.filemanager.components.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.todokanai.filemanager.R
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.todokanai.filemanager.adapters.NetRecyclerAdapter
 import com.todokanai.filemanager.databinding.FragmentNetBinding
 import com.todokanai.filemanager.viewmodel.NetViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,13 +16,24 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class NetFragment : Fragment() {
 
+    lateinit var listAdapter : NetRecyclerAdapter
     private val viewModel:NetViewModel by viewModels()
     private val binding by lazy{FragmentNetBinding.inflate(layoutInflater)}
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
+        listAdapter = NetRecyclerAdapter(
+            onItemClick = {viewModel.onItemClick(requireActivity(),it)},
+            itemFlow = viewModel.itemFlow
+        )
+        val verticalManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        binding.netRecyclerView.run{
+            adapter = listAdapter
+            layoutManager = verticalManager
+            addItemDecoration(DividerItemDecoration(context, verticalManager.orientation))
+        }
+
         return binding.root
     }
 }
--- app/src/main/java/com/todokanai/filemanager/viewmodel/NetViewModel.kt
+++ app/src/main/java/com/todokanai/filemanager/viewmodel/NetViewModel.kt
@@ -1,10 +1,15 @@
 package com.todokanai.filemanager.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
 class NetViewModel @Inject constructor() :ViewModel(){
-
+    val itemFlow: Flow<List<File>> = emptyFlow()
+    fun onItemClick(context: Context,item: File){}
 }
