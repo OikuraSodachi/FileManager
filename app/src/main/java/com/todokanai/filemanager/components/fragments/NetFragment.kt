dc565312c392638fb5f2e6bb6bb973b81267cf7c
Todokanai <bh951201@gmail.com>
2025-04-09 07:25:59 +0900

    NetFragmentLogic : abstract class 패턴 테스트 ( 도중에 로직이 옆길로 새는거 방지 목적 )
    Safe

--- app/src/main/java/com/todokanai/filemanager/components/fragments/NetFragment.kt
+++ app/src/main/java/com/todokanai/filemanager/components/fragments/NetFragment.kt
@@ -1,5 +1,6 @@
 package com.todokanai.filemanager.components.fragments
 
+import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,10 +10,12 @@ import kotlinx.coroutines.launch
 
 class NetFragment : NetFragmentLogic() {
 
-    override fun prepareView() {
-        netAdapter = NetRecyclerAdapter(
+    override val netAdapter: NetRecyclerAdapter
+        get() = NetRecyclerAdapter(
             onItemClick = {viewModel.onItemClick(it)}
         )
+
+    override fun prepareView() {
 
         val verticalManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
         binding.netRecyclerView.run{
@@ -30,7 +33,11 @@ class NetFragment : NetFragmentLogic() {
         }
     }
 
-    override fun backButtonCallback() {
-        viewModel.toParent()
+    override fun overrideBackButton() {
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                viewModel.toParent()
+            }
+        })
     }
 }
