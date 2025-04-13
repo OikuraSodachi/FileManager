2ed0e4d10e5def2e1f24698cf97c1ee1a3449744
Todokanai <bh951201@gmail.com>
2025-04-13 12:56:21 +0900

    NetFragment 를 FileListFragment 로 통합 진행중

--- app/src/main/java/com/todokanai/filemanager/adapters/DirectoryRecyclerAdapter.kt
+++ app/src/main/java/com/todokanai/filemanager/adapters/DirectoryRecyclerAdapter.kt
@@ -10,7 +10,17 @@ import java.io.File
 
 class DirectoryRecyclerAdapter(
     private val onClick: (File) -> Unit
-) : ListAdapter<File, DirectoryHolder>(DirectoryDiffCallback()) {
+) : ListAdapter<File, DirectoryHolder>(
+    object : DiffUtil.ItemCallback<File>() {
+        override fun areItemsTheSame(oldItem: File, newItem: File): Boolean {
+            return oldItem.absolutePath == newItem.absolutePath
+        }
+
+        override fun areContentsTheSame(oldItem: File, newItem: File): Boolean {
+            return oldItem == newItem
+        }
+    }
+) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DirectoryHolder {
         val view =
@@ -21,15 +31,4 @@ class DirectoryRecyclerAdapter(
     override fun onBindViewHolder(holder: DirectoryHolder, position: Int) {
         holder.onInit(getItem(position))
     }
-}
-
-private class DirectoryDiffCallback : DiffUtil.ItemCallback<File>() {
-    override fun areItemsTheSame(oldItem: File, newItem: File): Boolean {
-        return oldItem.absolutePath == newItem.absolutePath
-    }
-
-    override fun areContentsTheSame(oldItem: File, newItem: File): Boolean {
-        return oldItem == newItem
-    }
-
 }
