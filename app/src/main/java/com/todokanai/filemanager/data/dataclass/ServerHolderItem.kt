6a0fa57a006499e0247db23dedb1542237d22802
Todokanai <bh951201@gmail.com>
2025-05-12 13:43:38 +0900

    주석 추가.
    Safe

--- app/src/main/java/com/todokanai/filemanager/data/dataclass/ServerHolderItem.kt
+++ app/src/main/java/com/todokanai/filemanager/data/dataclass/ServerHolderItem.kt
@@ -1,5 +1,6 @@
 package com.todokanai.filemanager.data.dataclass
 
+/** Server Item 을 나타내는 View 를 위한 data class. data layer 에서 사용하지 말 것. **/
 data class ServerHolderItem(
     val name: String,
     val id: Long
